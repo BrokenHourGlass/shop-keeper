419077937819bf36ff69ac0385d917c131e43638
jacob <jacob.digby@skyrider.net>
2023-07-07 16:17:50 -0500

    added error handling on database to prevent index errors

--- Scripts/item_scene.gd
+++ Scripts/item_scene.gd
@@ -2,19 +2,21 @@ extends Node2D
 
 @export var item_id: int
 
+var item_data
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	item_id = 1
-	load_item_data(item_id)
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	item_data = load_item_data(item_id)
+	print(item_data)
 
 func load_item_data(id):
 	var file = FileAccess.open("res://data/item_database.json", FileAccess.READ)
 	var data = JSON.parse_string(file.get_as_text())
-	print(data["1"].name)
-	#print(item)
+	if data.has(str(id)):
+		return data[str(id)]
+	else:
+		return data[str(1)]
+	
+func set_item_id(value:int):
+	item_id = value
