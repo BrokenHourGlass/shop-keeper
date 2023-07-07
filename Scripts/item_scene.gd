1ab5457bd4bda2434c4bcddbfb46d0765cafcb9f
jacob <jacob.digby@skyrider.net>
2023-07-07 16:36:29 -0500

    gave script to user control created function for getting db specific data created methods for storing data in shop manager

--- Scripts/item_scene.gd
+++ Scripts/item_scene.gd
@@ -8,7 +8,6 @@ var item_data
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	item_data = load_item_data(item_id)
-	print(item_data)
 
 func load_item_data(id):
 	var file = FileAccess.open("res://data/item_database.json", FileAccess.READ)
@@ -20,3 +19,18 @@ func load_item_data(id):
 	
 func set_item_id(value:int):
 	item_id = value
+
+func get_base_value():
+	return item_data.base_value
+
+func get_item_name():
+	return item_data.name
+
+func get_item_rarity():
+	return item_data.rarity
+
+func get_item_type():
+	return item_data.type
+
+func get_sprite():
+	return item_data.sprite_path
--- Scripts/shop_scene.gd
+++ Scripts/shop_scene.gd
@@ -3,11 +3,22 @@ extends Node
 @onready var item_for_trade = $trade_item
 @export var trade_item_id:int = 1
 
+#trade values
+var agreed_trade_price: int
+
+#player values
+@export var cash_on_hand: int
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	get_trade_item(trade_item_id)
+	agreed_trade_price = item_for_trade.get_base_value()
+	print(item_for_trade.item_data)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func get_trade_item(trade_item_id):
+	item_for_trade.load_item_data(trade_item_id)
+	return item_for_trade
